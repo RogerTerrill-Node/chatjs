00a735d1827b4dd200ffb2d66c3c54a97e1364b9
Roger Terrill <rchicasterill@gmail.com>
2018-10-04 13:04:28 -0700

    Added style.css

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ app.listen(3000);
 console.log('App running...');
 
 function response(request, response) {
-    fs.readFile(__dirname + '/index.html',
-        function (error, data) {
-            if(error) {
-                response.writeHead(500);
-                return response.end('Failed to laod file index.html');
-            }
-
-            response.writeHead(200);
-            response.end(data);
+    fs.readFile(__dirname + '/index.html', function(error, data) {
+        if (error) {
+            response.writeHead(500);
+            return response.end('Failed to laod file index.html');
         }
-    );
+
+        response.writeHead(200);
+        response.end(data);
+    });
 }
