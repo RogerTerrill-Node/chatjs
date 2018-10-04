cc4b999e2233d42b83a07f34fd1b73b32b7b5c3b
Roger Terrill <rchicasterill@gmail.com>
2018-10-04 11:43:41 -0700

    Added index.html

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ app.listen(3000);
 console.log('App running...');
 
 function response(request, response) {
-    response.writeHead(200);
-    response.end('Soon enough, this will be a private chatroom for us!');
+    fs.readFile(__dirname + '/index.html',
+        function (error, data) {
+            if(error) {
+                response.writeHead(500);
+                return response.end('Failed to laod file index.html');
+            }
+
+            response.writeHead(200);
+            response.end(data);
+        }
+    );
 }
